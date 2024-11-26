b4c0c93958a3cd9b5c796f578a19ce87ac4e3ba7
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-11-26 16:34:14 +0530

    Updated

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,8 @@
+import { lusitana } from '@/app/ui/fonts';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
