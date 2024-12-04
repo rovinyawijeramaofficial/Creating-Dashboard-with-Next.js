bc61bad5fa0ecaa0c19e6162456b519c3c8d9275
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-12-04 09:31:44 +0530

    updated

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -8,7 +8,6 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { Button } from '@/app/ui/button';
 import { updateInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
@@ -27,6 +26,17 @@ export default function EditInvoiceForm({
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
+        {/* Display Validation Messages */}
+        {state.message && (
+          <p
+            className={`mb-4 text-sm ${
+              state.message.includes('Failed') ? 'text-red-600' : 'text-green-600'
+            }`}
+          >
+            {state.message}
+          </p>
+        )}
+
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -48,6 +58,9 @@ export default function EditInvoiceForm({
                 </option>
               ))}
             </select>
+            {state.errors?.customerId && (
+              <p className="text-sm text-red-600">{state.errors.customerId[0]}</p>
+            )}
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
         </div>
@@ -67,6 +80,9 @@ export default function EditInvoiceForm({
               placeholder="Enter USD amount"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
             />
+            {state.errors?.amount && (
+              <p className="text-sm text-red-600">{state.errors.amount[0]}</p>
+            )}
             <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
           </div>
         </div>
@@ -113,6 +129,9 @@ export default function EditInvoiceForm({
             </div>
           </div>
         </fieldset>
+        {state.errors?.status && (
+          <p className="mt-2 text-sm text-red-600">{state.errors.status[0]}</p>
+        )}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
